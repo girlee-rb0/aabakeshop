ae25bea4e07dc15a5a2dcc7ff71c43baba41fddb
girlee-rb0 <157123441+girlee-rb0@users.noreply.github.com>
2024-06-05 20:50:57 +0800

    Update orders.php

--- user/orders.php
+++ user/orders.php
@@ -4,7 +4,7 @@ include '../db_connection.php'; // Include database connection
 
 // Ensure the user is logged in
 if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php"); // Redirect to login page if not logged in
+    header("Location: ../login.php"); // Redirect to login page if not logged in
     exit;
 }
 
